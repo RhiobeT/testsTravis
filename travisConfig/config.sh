6dc83b3c8864c44fbe7dfa1e2b3ba64ec678ba8c
RhiobeT <dark.phoenix.62@gmail.com>
2015-03-10 14:45:22 +0100

    Config for generation

--- travisConfig/config.sh
+++ travisConfig/config.sh
@@ -3,8 +3,12 @@ sourceDir="$baseDir/srcdoc"
 sourceName="_source.adoc"
 stylesheetDir="$sourceDir/stylesheet"
 stylesheetName="asciidoctor.css"
-resultName="_result.html"
+resultHtmlName="_result.html"
+preResultName="_pre-result.html"
+resultMediawikiName="_result.mediawiki"
 htmlGenerationCommand=$(asciidoctor -a toc=macro -a data-uri -a stylesheet=$stylesheetName -a stylesdir=$stylesheetDir -o $resultName $sourceName)
+mediawikiGenerationCommand1=$(asciidoctor -a stylesheet=$stylesheetName -a numbered! -a stylesdir=$stylesheetDir -o $preResultName $sourceName)
+mediawikiGenerationCommand2=$(pandoc -f html -t mediawiki -o $resultMediawikiName $preResultName)
 
 # plantUML generation
 plantumlDir="$baseDir/tools/plantuml.jar"
