32af737692b35575c512cb53e14b47b083bc561b
RhiobeT <dark.phoenix.62@gmail.com>
2015-03-10 14:53:30 +0100

    Trying to do better with functions

--- travisConfig/config.sh
+++ travisConfig/config.sh
@@ -6,11 +6,27 @@ stylesheetName="asciidoctor.css"
 resultHtmlName="_result.html"
 preResultName="_pre-result.html"
 resultMediawikiName="_result.mediawiki"
-htmlGenerationCommand=$(asciidoctor -a toc=macro -a data-uri -a stylesheet=$stylesheetName -a stylesdir=$stylesheetDir -o $resultName $sourceName)
-mediawikiGenerationCommand1=$(asciidoctor -a stylesheet=$stylesheetName -a numbered! -a stylesdir=$stylesheetDir -o $preResultName $sourceName)
-mediawikiGenerationCommand2=$(pandoc -f html -t mediawiki -o $resultMediawikiName $preResultName)
+
+function htmlGenerationCommand {
+	asciidoctor -a toc=macro -a data-uri -a stylesheet=$stylesheetName -a stylesdir=$stylesheetDir -o $resultName $sourceName
+}
+
+function mediawikiGenerationCommand1 {
+	asciidoctor -a stylesheet=$stylesheetName -a numbered! -a stylesdir=$stylesheetDir -o $preResultName $sourceName
+}
+	
+function mediawikiGenerationCommand2 {
+	pandoc -f html -t mediawiki -o $resultMediawikiName $preResultName
+}
+
+
+
+
 
 # plantUML generation
 plantumlDir="$baseDir/tools/plantuml.jar"
 plantumlOutput="$baseDir/images"
-plantumlCommand=$(java -jar $plantumlDir -o $plantumlOutput *.plantuml)
+
+function plantumlCommand {
+	java -jar $plantumlDir -o $plantumlOutput *.plantuml)
+}
--- travisScripts/plantUML.sh
+++ travisScripts/plantUML.sh
@@ -3,7 +3,7 @@
 count=$(ls -1 *.plantuml 2>/dev/null | wc -l)
 
 if [ $count > 0 ] ; then
-	echo $(eval $plantumlCommand)
+	plantumlCommand
 else
 	echo 'No ".plantuml" file found'
 fi
