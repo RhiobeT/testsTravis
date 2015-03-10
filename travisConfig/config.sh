3133c94d72c9c139760b2cfef6af7a8e3a6d6583
RhiobeT <dark.phoenix.62@gmail.com>
2015-03-10 13:56:00 +0100

    Little test from the families

--- travisConfig/config.sh
+++ travisConfig/config.sh
@@ -1,3 +1,11 @@
+# documentation generation
+sourceDir="$baseDir/srcdoc"
+sourceName="_source.adoc"
+stylesheetDir="$srcDocDir/stylesheet"
+stylesheetName="asciidoctor.css"
+resultName="_result.html"
+htmlGenerationCommand=$(asciidoctor -a toc=macro -a data-uri -a stylesheet=$stylesheetName -a stylesdir=$stylesheetDir -o $resultName $sourceName)
+
 # plantUML generation
 plantumlDir="$baseDir/tools/plantuml.jar"
 plantumlOutput="images"
