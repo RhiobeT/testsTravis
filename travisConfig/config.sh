9be1e00176a029988114ad4cc3c6fedfd447cf59
RhiobeT <dark.phoenix.62@gmail.com>
2015-03-10 13:05:31 +0100

    Tests with the config file

---
+++ travisConfig/config.sh
@@ -0,0 +1,4 @@
+# plantUML generation
+$plantumlDir = $baseDir/tools/plantuml.jar
+$plantumlOutput = images
+$plantumlCommand = $(java -jar $plantumlDir -o $plantumlOutput *.plantuml)
--- travisScripts/plantUML.sh
+++ travisScripts/plantUML.sh
@@ -3,7 +3,7 @@
 count=`ls -1 *.plantuml 2>/dev/null | wc -l`
 
 if [ $count > 0 ] ; then
-	java -jar $baseDir/tools/plantuml.jar -o images *.plantuml
+	eval($plantumlCommand)
 else
 	echo 'No ".plantuml" file found'
 fi
