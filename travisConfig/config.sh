81ed03e45a5dad2b4c8f6a79810f6d39d8212ea6
RhiobeT <dark.phoenix.62@gmail.com>
2015-03-10 14:36:43 +0100

    Trying to fix with source

--- travisConfig/config.sh
+++ travisConfig/config.sh
@@ -5,15 +5,8 @@ stylesheetDir="$sourceDir/stylesheet"
 stylesheetName="asciidoctor.css"
 resultName="_result.html"
 htmlGenerationCommand=$(asciidoctor -a toc=macro -a data-uri -a stylesheet=$stylesheetName -a stylesdir=$stylesheetDir -o $resultName $sourceName)
-export sourceDir
-export sourceName
-export resultName
-export htmlGenerationCommand
 
 # plantUML generation
 plantumlDir="$baseDir/tools/plantuml.jar"
 plantumlOutput="$baseDir/images"
 plantumlCommand=$(java -jar $plantumlDir -o $plantumlOutput *.plantuml)
-export plantumlDir
-export plantumlOutput
-export plantumlCommand
