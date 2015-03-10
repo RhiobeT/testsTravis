5dbd0e43df1c35d9b57403e708a1375e9f007cfd
RhiobeT <dark.phoenix.62@gmail.com>
2015-03-10 23:49:24 +0100

    Debug

--- travisConfig/config.sh
+++ travisConfig/config.sh
@@ -69,6 +69,6 @@ toCommit=(
 
 function copyToCommit {
 	for element in $toCommit ; do
-		eval "cp --target-directory=$commitDir $element"
+		echo "--cp --target-directory=$commitDir $element"
 	done
 }
